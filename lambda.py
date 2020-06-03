6d79eca52809231262c29a954f382d81dfd9e188
Ashish S <ashishjsharda@gmail.com>
2020-06-03 05:31:14 -0400

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#lambda expression with 2 argument
+add =lambda x,y:x+y
+print(add(10,20))
+#Lambda expression with 1 argument
+hello=lambda :"Hello"
+print(hello())
+show=lambda w: w
+print(show("Hi"))
