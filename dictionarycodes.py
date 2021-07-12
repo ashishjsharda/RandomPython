dab57808665ee896b5b5595b96708b8727d2d81b
Ashish S <ashishjsharda@gmail.com>
2021-07-12 14:39:17 -0400

    Create dictionarycodes.py

---
+++ dictionarycodes.py
@@ -0,0 +1,9 @@
+dial_codes=[
+    (880,'Bangladesh'),
+    (91,'India'),
+    (1,'USA'),
+    (7,'Russia'),
+    (92,'Pakistan')
+]
+country_dial={country:code for code,country in dial_codes}
+print(country_dial)
